2a998b3c3946453ac0db3beee62f3485791ae09e
João Toledo <joaovtsilva5a@gmail.com>
2025-10-08 22:19:12 -0300

    Inicio do projeto

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>Hello World</p>
+        <p>Iniciando o Desenvolvimento do Projeto</p>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
